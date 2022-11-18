ff4f3e29003654f897afc2a32a98a2407b3f2976
teddyatkinson <tedddy.atkinson@gmail.com>
2022-11-18 13:03:49 -0800

    WORKING: Debuging error handling in api service request

--- src/currencyfunc.js
+++ src/currencyfunc.js
@@ -3,24 +3,23 @@ import { UserSearch } from "./apiservice.js";
 
 export async function display(event) {
   event.preventDefault();
+  document.getElementById("conversionDisplay").innerHTML = null;
   const dollarInput = document.getElementById("dollarInput").value;
   const convCurrency = document.getElementById("convCurrency").value;
-  const newSearch = new UserSearch(dollarInput,convCurrency);
+  const newSearch = new UserSearch(dollarInput, convCurrency);
   const dataRequest = await newSearch.getData();
-  if (dataRequest.meta.found === 0) {
+
+  if (dataRequest.result === "error") {
     swal({
       title: "ERROR:",
-      text: "INPUT CURRENCY NOT FIELDED",
+      text: "CURRENCY CONVERSION NOT SUPPORTED",
       icon: "error",
       button: "OK",
     });
   } else {
-    // dataRequest.data.forEach((article) => {
-    //   const title = article["title"];
-    //   const text = article["snippet"];
-    //   const imgURL = article["image_url"];
-    //   const linkURL = article["url"];
-    //   appendElements(title, text, imgURL, linkURL);
-    // });
+    const conversionOutput = dataRequest.conversion_result;
+    let currencydisplay = document.getElementById("conversionDisplay");
+    currencydisplay.setAttribute("class", "center");
+    currencydisplay.innerHTML = conversionOutput;
   }
 }
